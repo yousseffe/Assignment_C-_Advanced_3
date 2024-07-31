231a094a35e79c47f33b70299831d9e78c30a52e
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-07-31 18:29:36 +0300

    Assignment

---
+++ book/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_C__Advanced_3.book
+{
+
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+        public override string ToString()
+        {
+            string authors = string.Join(", ", Authors);
+            return $"ISBN: {ISBN}, Title: {Title}, Authors: {authors}, Publication Date: {PublicationDate.ToShortDateString()}, Price: {Price:C}";
+        }
+    }
+}
